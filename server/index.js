c2453f3eef9bcfe80b55bec38185be4e99c0131f
anoize <s6603052413108@email.kmutnb.ac.th>
2025-03-05 15:44:33 +0700

    server index.js

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const app=express()
+const port=8000
+
+app.get('/',(request,response)=>{
+    response.send(`
+    <!doctype html>
+    <html>
+        <head>
+            <title>
+                Express.ja
+            </title>
+        </head>
+        <body>
+            <h3>Welcome to Express.js</h3>
+            <b>Express.ja Fast, unopinionated, minimalist <br>
+            web framework for Node.js</b>
+        </body>
+    </html>`)
+})
+app.listen(port,()=> console.log('Server listening on port'+port))
+
+
+
